f3c82078c745e0797c20d34bdfea08475874098f
trnhatnam <trinhnhatnam75@gmail.com>
2023-12-16 10:39:30 +0100

    nouveau main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include "tilemap.hh"
+#include "jeu.hpp"
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     // on crée la fenêtre
+    srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode(960, 960), "Tilemap");
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(false);
-
     // on définit le niveau à l'aide de numéro de tuiles
     int level[] =
     {
@@ -29,23 +32,8 @@ int main()
     };
 
     // on crée la tilemap avec le niveau précédemment défini
-    // pour simuler une tilemap infini, on crée 2 tilemap
-    TileMap map;
-    TileMap map2;
-    TileMap map3;
-    sf::View view = window.getView();
-    map.setPosition(0.f, 0.f);
-    map2.setPosition(0.f, -960.f);
-    map3.setPosition(0.f, -960.f*2);
-
-    if (!map.load("tileset.png", sf::Vector2u(64, 64), level, 15, 15))
-        return -1;
-    
-    if (!map2.load("tileset.png", sf::Vector2u(64, 64), level, 15, 15))
-        return -1;
-
-    if (!map3.load("tileset.png", sf::Vector2u(64, 64), level, 15, 15))
-        return -1;
+    // pour simuler une tilemap infini, on crée 3 tilemap
+    Jeu jeu(level); 
 
     // sprite
     sf::Sprite voiture;
@@ -56,6 +44,10 @@ int main()
     voiture.setPosition(sf::Vector2f(320.f, 600.f));
     float vitesse = 10.f;
 
+    sf::Clock clock;
+    bool tree = true;
+    sf::Texture arbre;
+    
     // on fait tourner la boucle principale
     while (window.isOpen())
     {
@@ -66,17 +58,16 @@ int main()
             if(event.type == sf::Event::Closed)
                 window.close();
         }
-
-        if  (map.getPosition().y > 960.f - vitesse)
-            map.setPosition(0.f, -960.f);
-        if  (map2.getPosition().y > 960.f - vitesse)
-            map2.setPosition(0.f, -960.f);
-        if  (map3.getPosition().y > 960.f - vitesse)
-            map3.setPosition(0.f, -960.f);
         
-        map.move(0.f,vitesse);
-        map2.move(0.f,vitesse);
-        map3.move(0.f,vitesse);
+        if (clock.getElapsedTime().asSeconds() > 5.f)
+        {
+            jeu.spawn_obstacle(arbre);
+            clock.restart();
+        }
+
+        jeu.clear();
+        jeu.defiler(vitesse);
+        
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             voiture.move(-3.f, 0.f);
@@ -84,14 +75,10 @@ int main()
             voiture.move(3.f, 0.f);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
             window.close();
-        
-
 
         // on dessine le niveau
         window.clear();
-        window.setView(view);
-        window.draw(map);
-        window.draw(map2);
+        jeu.drawIn(window);
         window.draw(voiture);
         window.display();
     }
