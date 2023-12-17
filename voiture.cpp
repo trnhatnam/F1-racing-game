c5d9c4453399d3ade3b46188f6ace76fde2b0956
PolonaisBreton <114663299+PolonaisBreton@users.noreply.github.com>
2023-12-17 03:17:49 +0100

    Add files via upload

--- voiture.cpp
+++ voiture.cpp
@@ -1,13 +1,27 @@
 #include "voiture.hpp"
 
-Voiture::Voiture(float x, float y, float speed, float maxSpeed) : _x(x), _y(y), _speed(speed), _maxSpeed(maxSpeed) {}
+Voiture::Voiture(float x, float y, float speed, float maxSpeed, const sf::Texture& texture)
+    : _speed(speed), _maxSpeed(maxSpeed) {
+    // mise en place de la texture
+    sprite.setTexture(texture);
+    // positionnement de la voiture
+    sprite.setPosition(x, y);
+    // mise a l'echelle du sprite de la voiture
+    sprite.setScale(1.5f,1.5f);
+}
+
+void Voiture::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    states.transform *= getTransform();
+    // dessine le sprite de la voiture avec les transformations appliquées
+    target.draw(sprite, states);
+}
 
 float Voiture::getX() const {
-    return _x;
+    return sprite.getPosition().x;
 }
 
 float Voiture::getY() const {
-    return _y;
+    return sprite.getPosition().y;
 }
 
 float Voiture::getSpeed() const {
@@ -18,16 +32,16 @@ float Voiture::getMaxSpeed() const {
     return _maxSpeed;
 }
 
-void Voiture::move(float sideStep) {
-    _x += sideStep;
+void Voiture::move(float offsetX, float offsetY) {
+    sprite.move(offsetX, offsetY);
 }
 
 void Voiture::startSpeedUp() {
     _speed += 5.0;
 }
 
-void Voiture::SpeedUp(){
+void Voiture::SpeedUp() {
     _speed += 0.01;
-    if(_speed > _maxSpeed)
+    if (_speed > _maxSpeed)
         _speed = _maxSpeed;
 }
