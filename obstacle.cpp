c88a0e66596a52d469d4dabc988a3a9ec4e77d43
trnhatnam <trinhnhatnam75@gmail.com>
2023-12-28 11:27:05 +0100

    ajout des autres obstacles

--- obstacle.cpp
+++ obstacle.cpp
@@ -4,16 +4,18 @@ Obstacle::Obstacle()
 {
     if (!isTexturesInitialized)
     {
-        for (int i=0; i<3; i++){
+        for (int i=0; i<6; i++){
             texturesObstacles.push_back(sf::Texture());
         }
         if (!texturesObstacles[0].loadFromFile("assets/tree.png", sf::IntRect(0, 0, 64, 64)) ||
             !texturesObstacles[1].loadFromFile("assets/barrier.png", sf::IntRect(0, 0, 64, 64)) ||
-            !texturesObstacles[2].loadFromFile("assets/pompe1.png", sf::IntRect(0, 0, 64, 64)))
+            !texturesObstacles[2].loadFromFile("assets/pompe1.png", sf::IntRect(0, 0, 64, 64)) ||
+            !texturesObstacles[3].loadFromFile("assets/pompe2.png", sf::IntRect(0, 0, 64, 64)) ||
+            !texturesObstacles[4].loadFromFile("assets/pompe3.png", sf::IntRect(0, 0, 64, 64)) ||
+            !texturesObstacles[5].loadFromFile("assets/pompe4.png", sf::IntRect(0, 0, 64, 64)))
             printf("Erreur chargement des textures d'obstacles");
         isTexturesInitialized = true;
     }
-    setTexture(texturesObstacles[rand() % 3]);
-    vitesseRelative = 0;
+    setTexture(texturesObstacles[rand() % 6]);
     // initialiser résistance et tout aléatoirement pour que ça soit plus fun
 }
